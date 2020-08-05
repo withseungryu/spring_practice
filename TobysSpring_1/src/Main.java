c5a5f9ccc0957fa8c18d7a0bd2d792b5ffe8514e
withseungryu <alstmdsha@gmail.com>
2020-08-05 16:20:54 +0900

    초난감 User DAO 객체

---
+++ TobysSpring_1/src/Main.java
@@ -0,0 +1,28 @@
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		System.out.println("gd");
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("minseung Park");
+        user.setPassword("married");
+
+        dao.add(user);
+        
+        System.out.println(user.getId() + "등록 성공");
+        
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        
+        System.out.println(user2.getPassword());
+        
+        System.out.println(user2.getId() + "조회 성공");
+        System.out.println("git check");
+	}
+
+}
