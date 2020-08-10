b8ce75a61a26d98a37eef1f45b3739de790b3eb1
withseungryu <alstmdsha@gmail.com>
2020-08-10 19:37:40 +0900

    XML을 이용하는 애플리케이션 컨텍스트와 테스트를 위한 애플리케이션 컨텍스트 관리 @Runwith, @ContextConfiguration, @Autowired 추가

--- TobysSpring_1/src/UserDaoTest.java
+++ TobysSpring_1/src/UserDaoTest.java
@@ -6,11 +6,25 @@ import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;;
 
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:applicationContext.xml")
 public class UserDaoTest {
+	
+	@Autowired
+	private ApplicationContext context;
 	
 	private UserDao dao;
 	private User user1;
@@ -20,8 +34,8 @@ public class UserDaoTest {
 	
 	@Before
 	public void setUp() {
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		
+	
+//		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		this.dao = context.getBean("userDao", UserDao.class);
 		
 		this.user1 = new User("gyumee", "sungcheol Park", "springno1");
