7d70a2678ab8f624982a4dce9cb5d366ab7bab5b
withseungryu <alstmdsha@gmail.com>
2020-08-11 13:52:46 +0900

    JUnit 테스트 오브젝트 테스트

---
+++ TobysSpring_1/src/JUnitTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class JUnitTest {
+	static JUnitTest testObject;
+	
+	@Test 
+	public void test1() {
+		assertThat(this, is(not(sameInstance(testObject))));
+		testObject = this;
+	}
+	
+	@Test
+	public void test2() {
+		assertThat(this, is(not(sameInstance(testObject))));
+		testObject = this;
+	}
+	
+	@Test
+	public void test3() {
+		assertThat(this, is(not(sameInstance(testObject))));
+		testObject = this;
+	}
+}
