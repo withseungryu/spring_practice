37b088dc7c5937e191b7e3da997216d0f3290e66
withseungryu <alstmdsha@gmail.com>
2020-08-11 14:01:09 +0900

    Set을 이용해서 모든 오브젝트 비교 코드

--- TobysSpring_1/src/JUnitTest.java
+++ TobysSpring_1/src/JUnitTest.java
@@ -3,25 +3,35 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class JUnitTest {
-	static JUnitTest testObject;
+//	static JUnitTest testObject;
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 	
 	@Test 
 	public void test1() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test
 	public void test2() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+//		assertThat(this, is(not(sameInstance(testObject))));
+//		testObject = this;
+		
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test
 	public void test3() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+//		assertThat(this, is(not(sameInstance(testObject))));
+//		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 }
