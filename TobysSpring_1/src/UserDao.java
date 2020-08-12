bf75070cc3b147bc08ada6f25d1262100b2c3aaa
withseungryu <alstmdsha@gmail.com>
2020-08-12 15:21:58 +0900

    로컬클래스로 add 메소드 안에 AddStatement클래스를 만들어줌.

--- TobysSpring_1/src/UserDao.java
+++ TobysSpring_1/src/UserDao.java
@@ -15,9 +15,18 @@ public class UserDao {
 		this.dataSource = dataSource;
 	}
 	
-	public void add(User user) throws ClassNotFoundException, SQLException {
-        
-		StatementStrategy st = new AddStatement(user);
+	public void add(final User user) throws ClassNotFoundException, SQLException {
+        class AddStatement implements StatementStrategy{
+        	public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+        		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
+        		 ps.setString(1, user.getId());
+        	     ps.setString(2, user.getName());
+        	     ps.setString(3, user.getPassword());
+        	     return ps;
+        	}
+        }
+		
+		StatementStrategy st = new AddStatement();//따로 생성자를 만들어줄 필요가 없다 로컬클래스이므로
 		jdbcContextWithStatementStrategy(st);
 
     }
