30e55e8eabba1508e672364a11c0f84a88e600fe
withseungryu <alstmdsha@gmail.com>
2020-08-12 14:48:14 +0900

    DI적용을 위한 클라이언트/컨텍스트 분리

--- TobysSpring_1/src/UserDao.java
+++ TobysSpring_1/src/UserDao.java
@@ -61,35 +61,8 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		Connection c = null;
-		//dataSource.getConnection();
-		
-		PreparedStatement ps = null;
-		//c.prepareStatement("delete from users");
-		
-		try {
-			c = dataSource.getConnection();
-			StatementStrategy strategy = new DeleteAllStatement();
-			ps=strategy.makePreparedStatement(c);
-			ps.executeUpdate();
-		}catch(SQLException e) {
-			throw e;
-		}finally {
-			if(ps != null) {
-				try {
-					ps.close();
-				}catch(SQLException e) {
-					
-				}
-			}
-			if(c != null) {
-				try {
-					c.close();
-				}catch(SQLException e) {
-					
-				}
-			}
-		}
+		StatementStrategy st = new DeleteAllStatement();
+		jdbcContextWithStatementStrategy(st);
 		
 	}
 	
@@ -136,6 +109,35 @@ public class UserDao {
 			}
 		}
 
+	}
+	
+	//컨텍스트 부분을 분리하기 위한 메소드
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		
+		try {
+			c = dataSource.getConnection();
+			ps =stmt.makePreparedStatement(c);
+			ps.executeUpdate();
+		}catch(SQLException e) {
+			throw e;
+		}finally {
+			if(ps != null) {
+				try {
+					ps.close();
+				}catch(SQLException e) {
+					
+				}
+			}
+			if(c != null) {
+				try {
+					c.close();
+				}catch(SQLException e) {
+					
+				}
+			}
+		}
 	}
 		
 
