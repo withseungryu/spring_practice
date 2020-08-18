1a93feb7610dc45eac1dd56fc40678f846cf3f61
withseungryu <alstmdsha@gmail.com>
2020-08-18 14:21:16 +0900

    jdbcTempalte의 queryForObject를 사용해 UserDao의 get() 메소드 구현

--- TobysSpring_1/src/UserDao.java
+++ TobysSpring_1/src/UserDao.java
@@ -10,6 +10,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 
 public class UserDao {
@@ -76,33 +77,48 @@ public class UserDao {
 
     }
 	
-	public User get(String id) throws ClassNotFoundException, SQLException {
+	public User get(String id) throws ClassNotFoundException, SQLException, EmptyResultDataAccessException {
 	
-		Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id = ?");
-		ps.setString(1,  id);
+//		Connection c = dataSource.getConnection();
+//        PreparedStatement ps = c.prepareStatement(
+//                "select * from users where id = ?");
+//		ps.setString(1,  id);
+//		
+//		ResultSet rs = ps.executeQuery();
+//		
+//		User user = null;
+//		
+//		if(rs.next()) {
+//			user = new User();
+//			user.setId(rs.getString("id"));
+//			user.setName(rs.getString("name"));
+//			user.setPassword(rs.getString("password"));
+//		}
+//		
+//		
+//		rs.close();
+//		ps.close();
+//		c.close();
+//		
+//		if(user==null) throw new EmptyResultDataAccessException(1);
+//        
+//        return user;
 		
-		ResultSet rs = ps.executeQuery();
-		
-		User user = null;
-		
-		if(rs.next()) {
-			user = new User();
-			user.setId(rs.getString("id"));
-			user.setName(rs.getString("name"));
-			user.setPassword(rs.getString("password"));
-		}
-		
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		if(user==null) throw new EmptyResultDataAccessException(1);
-        
-        return user;
-	}
+		return this.jdbcTemplate.queryForObject("select * from users where id =?", 
+				new Object[] {id},
+				new RowMapper<User>() {
+					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+						User user = new User();
+						user.setId(rs.getString("id"));
+						user.setName(rs.getString("name"));
+						user.setPassword(rs.getString("password"));
+						return user;
+					}
+			
+		});
+				
+	}	
+	
 	
 	public void deleteAll() throws SQLException {
 //		StatementStrategy st = new DeleteAllStatement();
@@ -175,10 +191,10 @@ public class UserDao {
 //		});
 		
 	
-		return this.jdbcTemplate.queryForInt("select (*) from users");
-		
+			return this.jdbcTemplate.queryForInt("select (*) from users");
+		}
 
-	}
+	
 	
 	//컨텍스트 부분을 분리하기 위한 메소드
 //	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
