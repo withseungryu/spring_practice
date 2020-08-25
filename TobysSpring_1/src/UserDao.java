d1859604a5f588b53563413199f2dc8140b2fc0d
withseungryu <alstmdsha@gmail.com>
2020-08-25 14:26:34 +0900

    UserDaoJdbc에 update 메소드를 추가, 이 메소드 테스트 코드 추가

--- TobysSpring_1/src/UserDao.java
+++ TobysSpring_1/src/UserDao.java
@@ -8,4 +8,5 @@ public interface UserDao {
 	List<User> getAll();
 	void deleteAll();
 	int getCount();
+	void update(User user1);
 }
--- TobysSpring_1/src/UserDaoJdbc.java
+++ TobysSpring_1/src/UserDaoJdbc.java
@@ -74,6 +74,7 @@ public class UserDaoJdbc implements UserDao{
 		this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend) values(?,?,?,?,?,?)", user.getId(), user.getName(), user.getPassword()
 				, user.getLevel().intValue(), user.getLogin(), user.getRecommend()
 				);
+		
 
     }
 	
@@ -125,16 +126,11 @@ public class UserDaoJdbc implements UserDao{
 	
 	public void deleteAll() {
 //		StatementStrategy st = new DeleteAllStatement();
+
 		this.jdbcTemplate.update(
-//				new PreparedStatementCreator() {
-//					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//						return con.prepareStatement("delete from users");
-//					}
-//				}
-//				
 				"delete from users"
 				);
-		
+	
 	}
 	
 
@@ -242,6 +238,18 @@ public class UserDaoJdbc implements UserDao{
 			}
 		}
 				);
+	}
+
+
+
+	public void update(User user1) {
+	
+		this.jdbcTemplate.update(
+				"update users set name = ?, password = ?, level = ?, login = ?, recommend = ? where id= ?", 
+				user1.getName(), user1.getPassword(), user1.getLevel().intValue(), user1.getLogin(), user1.getRecommend(), user1.getId()
+				);
+				
+		System.out.println("gd");
 	}
 
 }
