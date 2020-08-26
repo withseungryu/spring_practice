a304fc5ea8cc814c6bb9aed3efb8be49627c3da5
withseungryu <alstmdsha@gmail.com>
2020-08-26 19:38:21 +0900

    코드 리팩토링으로 인한 Test 코드 수정

--- TobysSpring_1/src/UserServiceTest.java
+++ TobysSpring_1/src/UserServiceTest.java
@@ -55,16 +55,20 @@ public class UserServiceTest {
 		
 		userService.upgradeLevels();
 		
-		checkLevel(users.get(0), Level.BASIC);
-		checkLevel(users.get(1), Level.SILVER);
-		checkLevel(users.get(2), Level.SILVER);
-		checkLevel(users.get(3), Level.GOLD);
-		checkLevel(users.get(4), Level.GOLD);
+		checkLevel(users.get(0), false);
+		checkLevel(users.get(1), true);
+		checkLevel(users.get(2), false);
+		checkLevel(users.get(3), true);
+		checkLevel(users.get(4), false);
 	}
 	
-	private void checkLevel(User user, Level expectedLevel) {
+	private void checkLevel(User user,boolean upgraded) {
 		User userUpdate = userDao.get(user.getId());
-		assertThat(userUpdate.getLevel(), is(expectedLevel));
+		if(upgraded) {
+			assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+		}else{
+			assertThat(userUpdate.getLevel(), is(user.getLevel()));
+		}
 	}
 	
 	@Test
